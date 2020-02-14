58d20e3f287f0a2c14eb51145e160bf8c115877d
Rachel Schieferstein <violenza@gmail.com>
2020-02-14 12:17:45 -0800

    adding output of address within search radius parameters

--- src/main.js
+++ src/main.js
@@ -24,12 +24,17 @@ $(document).ready(function() {
         document.getElementById("doctors").innerHTML = "There are no results for this search."
       }
       if (response) {
-        // document.getElementById("doctors").innerHTML = "Working. technically"
         for(let i=0; i < response.data.length; i++) {
           $('#doctors').append(`${response.data[i].profile.first_name} ${response.data[i].profile.last_name}, ${response.data[i].profile.title}<br>`);
+          
+          for(let j=0; j < response.data[i].practices.length; j++) {
+            if (response.data[i].practices[j].within_search_area === true) {
+              $('#doctors').append(`Address: <em>${response.data[i].practices[j].visit_address.street}, ${response.data[i].practices[j].visit_address.city}, ${response.data[i].practices[j].visit_address.state}, ${response.data[i].practices[j].visit_address.zip}</em><br>`);
+            }
+          }
         }
       } else {
-        document.getElementById("doctors").innerHTML = "Something went wrong with this request."
+        document.getElementById("doctors").innerHTML = "Something went wrong with this request.";
       }
     }
 
