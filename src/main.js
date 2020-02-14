751a3b5e98060767a6c9ac9ea1a362346c01bb09
Rachel Schieferstein <violenza@gmail.com>
2020-02-14 13:26:44 -0800

    debugging 'no results' output conditions

--- src/main.js
+++ src/main.js
@@ -20,25 +20,33 @@ $(document).ready(function() {
     })();
 
     function getElements(response) {
-      if (response.data.length == 0) {
-        document.getElementById("doctors").innerHTML = "There are no results for this search."
-      }
+
       if (response) {
+        if (response.data.length == 0) {
+          document.getElementById("doctors").innerHTML = "There are no results for this search."
+        }
         for(let i=0; i < response.data.length; i++) {
-          $('#doctors').append(`<strong>${response.data[i].profile.first_name} ${response.data[i].profile.last_name}, ${response.data[i].profile.title}</strong><br><br>`);
-          
+          $('#doctors').append(`<strong>${response.data[i].profile.first_name} ${response.data[i].profile.last_name}, ${response.data[i].profile.title}</strong><br>`);
+            if (response.data[i].specialties[0] != undefined) {
+              $('#doctors').append(`<em>${response.data[i].specialties[0].actor}</em><br><br>`);
+            }
           for(let j=0; j < response.data[i].practices.length; j++) {
             if (response.data[i].practices[j].within_search_area === true) {
-              $('#doctors').append(`${response.data[i].practices[j].name}<br>Address: <em>${response.data[i].practices[j].visit_address.street}, ${response.data[i].practices[j].visit_address.city}, ${response.data[i].practices[j].visit_address.state}, ${response.data[i].practices[j].visit_address.zip}</em><br>`);
+              $('#doctors').append(`Practice: <strong>${response.data[i].practices[j].name}</strong><br>Address: <em>${response.data[i].practices[j].visit_address.street}, ${response.data[i].practices[j].visit_address.city}, ${response.data[i].practices[j].visit_address.state}, ${response.data[i].practices[j].visit_address.zip}</em>`);
+                if(response.data[i].practices[j].accepts_new_patients == true) {
+                  $('#doctors').append('<br>Accepting new patients?: <strong>Yes</strong><br>')
+                } else {
+                  $('#doctors').append('<br>Accepting new patients?: <strong>No</strong><br>')
+                }
               
               for(let k=0; k < response.data[i].practices[j].phones.length; k++) {
                 if (response.data[i].practices[j].phones[k].type == "landline") {
                   $('#doctors').append(`Phone: ${response.data[i].practices[j].phones[k].number}<br><br>`);
                 }
-
               }
             }
           }
+
         }
       } else {
         document.getElementById("doctors").innerHTML = "Something went wrong with this request.";
