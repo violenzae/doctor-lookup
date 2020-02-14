a44eabab21626559687e35d8f1192bd05df4f2db
Rachel Schieferstein <violenza@gmail.com>
2020-02-14 11:18:55 -0800

    adding message for no search results

--- src/main.js
+++ src/main.js
@@ -20,14 +20,14 @@ $(document).ready(function() {
     })();
 
     function getElements(response) {
-      if (response.meta.count == 0) {
+      if (response.data.length == 0) {
         document.getElementById("doctors").innerHTML = "There are no results for this search."
       }
       if (response) {
-        document.getElementById("doctors").innerHTML = "Working. technically"
-        // for(let i=0; i < response.meta.count.length; i++) {
-        //   // $('#doctors').append(`Dr. ${city} is ${response.main.humidity}%`);
-        // }
+        // document.getElementById("doctors").innerHTML = "Working. technically"
+        for(let i=0; i < response.meta.count.length; i++) {
+          // $('#doctors').append(`Dr. ${city} is ${response.main.humidity}%`);
+        }
       } else {
         document.getElementById("doctors").innerHTML = "Something went wrong with this request."
       }
